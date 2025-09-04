75475dd2b1519ee60ac452f4253de63b593e913d
wonswe <64626054+wonswe@users.noreply.github.com>
2025-09-04 12:22:27 -0700

    add addBtn

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const addBtn = document.querySelector('.footer__button');
 
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아 옴
+  const text = input.value;
+  console.log(text);
   // 2. 새로운 아이템을 만듦 (텍스트 + 삭제 버튼)
+  //   const item = createItem();
   // 3. items container안에 새로 만든 아이템을 추가
+  //   items.appendChild(item);
   // 4. input을 초기화 한다.
+  input.value = '';
+  input.focus();
 }
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});

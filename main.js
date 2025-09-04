6e36260d363b2ba890b72b366704af2f7ebf939e
wonswe <64626054+wonswe@users.noreply.github.com>
2025-09-04 12:18:46 -0700

    add basic js

---
+++ main.js
@@ -0,0 +1,10 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+function onAdd() {
+  // 1. 사용자가 입력한 텍스트를 받아 옴
+  // 2. 새로운 아이템을 만듦 (텍스트 + 삭제 버튼)
+  // 3. items container안에 새로 만든 아이템을 추가
+  // 4. input을 초기화 한다.
+}

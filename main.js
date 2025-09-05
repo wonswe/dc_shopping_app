0d9cf7313f374f24cf6e97ae54c6f2705ae1e953
wonswe <64626054+wonswe@users.noreply.github.com>
2025-09-05 12:25:43 -0700

    add addBtn to add items to the list

--- main.js
+++ main.js
@@ -7,12 +7,41 @@ function onAdd() {
   const text = input.value;
   console.log(text);
   // 2. 새로운 아이템을 만듦 (텍스트 + 삭제 버튼)
-  //   const item = createItem();
+  const item = createItem(text);
   // 3. items container안에 새로 만든 아이템을 추가
-  //   items.appendChild(item);
+  items.appendChild(item);
   // 4. input을 초기화 한다.
   input.value = '';
   input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
 }
 
 addBtn.addEventListener('click', () => {

ecf3ea74058aa7422176f68cb219411f56f30cd4
wonswe <64626054+wonswe@users.noreply.github.com>
2025-09-25 10:22:33 -0400

    remove redundant comments

--- main.js
+++ main.js
@@ -3,20 +3,15 @@ const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 
 function onAdd() {
-  // 1. 사용자가 입력한 텍스트를 받아 옴
   const text = input.value;
   if (text === '') {
     input.focus();
     return;
   }
   console.log(text);
-  // 2. 새로운 아이템을 만듦 (텍스트 + 삭제 버튼)
   const item = createItem(text);
-  // 3. items container안에 새로 만든 아이템을 추가
   items.appendChild(item);
-  // 4. 새로 추가된 아이템으로 스크롤링
   item.scrollIntoView({ block: 'center' });
-  // 5. input을 초기화 한다.
   input.value = '';
   input.focus();
 }

f34e1631383fa9c84687d71def070ea05a5e88dd
wonswe <64626054+wonswe@users.noreply.github.com>
2025-10-01 00:04:00 -0400

    updated main.js

--- main.js
+++ main.js
@@ -15,33 +15,46 @@ function onAdd() {
   input.value = '';
   input.focus();
 }
+let id = 0; // UUID or object's hash
 
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  itemRow.innerHTML = `
+    <div class="item data-id=${id}>
+      <span class="item__name">${text}</span>
+      <button class="item__delete">
+        <i class="fas fa-trash-alt" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>
+  `;
 
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
+  // const name = document.createElement('span');
+  // name.setAttribute('class', 'item__name');
+  // name.innerText = text;
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'item__delete');
+  // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  // deleteBtn.addEventListener('click', () => {
+  //   items.removeChild(itemRow);
+  // });
 
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item__divider');
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  // item.appendChild(name);
+  // item.appendChild(deleteBtn);
+
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
+  id++;
   return itemRow;
 }
 
@@ -52,5 +65,13 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     onAdd();
+  }
+});
+
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id && event.target.tagName === 'I') {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
